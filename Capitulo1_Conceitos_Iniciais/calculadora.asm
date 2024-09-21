449222bf9a534c0b4b31f56c43f6e8e2b2073c5c
Danilo <danilogmoura@gmail.com>
2024-09-21 12:50:51 -0300

    Update calculadora.asm

--- Capitulo1_Conceitos_Iniciais/calculadora.asm
+++ Capitulo1_Conceitos_Iniciais/calculadora.asm
@@ -1,32 +1,32 @@
 %include "../Biblioteca/bibliotecaE.inc"
 
 section .data
-    titulo              db  LF, "+-------------+", LF, \
-                                "| Calculadora |", LF, \
-                                "+-------------+", STR_END
+    titulo              db      LF, "+-------------+", LF, \
+                                    "| Calculadora |", LF, \
+                                    "+-------------+", STR_END
 
-    valor_1             db  LF, "Valor 1:", STR_END
-    valor_2             db  LF, "Valor 2:", STR_END
+    msg_valor_1         db      "Valor 1: ", STR_END
+    msg_valor_2         db      "Valor 2: ", STR_END
 
-    opc_adicao          db  LF, "1. Adicionar", STR_END
-    opc_subtracao       db  LF, "2. Subtração", STR_END
-    opc_divisao         db  LF, "3. Divisão", STR_END
-    opc_multiplicacao   db  LF, "4. Multiplicação", STR_END
+    opc_adicao          db      LF, "1. Adição", STR_END
+    opc_subtracao       db      LF, "2. Subtração", STR_END
+    opc_divisao         db      LF, "3. Divisão", STR_END
+    opc_multiplicacao   db      LF, "4. Multiplicação", STR_END
 
-    msg_opcao           db  LF, LF, "Deseja Realizar? ", STR_END
-    msg_erro            db  LF, "Valor da Opção Inválido", STR_END
+    msg_opcao           db      LF, LF, "Qual oepração deseja realizar? ", STR_END
+    msg_erro            db      LF, "Valor da Opção Inválido", STR_END
 
-    proc_adicao         db  LF, "Processo Adicionar", STR_END
-    proc_subtracao      db  LF, "Processo Subtração", STR_END
-    proc_divisao        db  LF, "Processo Divisão", STR_END
-    proc_multiplicacao  db  LF, "Processo Multiplicação", STR_END
+    proc_adicao         db      LF, "Processo de Adição", STR_END
+    proc_subtracao      db      LF, "Processo de Subtração", STR_END
+    proc_divisao        db      LF, "Processo de Divisão", STR_END
+    proc_multiplicacao  db      LF, "Processo de Multiplicação", STR_END
 
-    msg_fim             db  LF, "Terminei.", LF, STR_END
+    msg_fim             db      LF, "Terminei.", LF, STR_END
 
 section .bss
-    opcao       resb    0x1
-    numero_1    resb    0x1
-    numero_2    resb    0x1
+    opcao               resb    0x1
+    valor_1             resb    0x1
+    valor_2             resb    0x1
 
 section .text
 
@@ -57,8 +57,73 @@ _start:
     int INT_80H
 
     mov ah, [opcao]
+    sub ah, "0"
+
+    cmp ah, 0x4
+    jg mensagem_erro
+    cmp ah, 0x1
+    jl mensagem_erro
+
+    mov ecx, msg_valor_1
+    call imprimir_string
+    mov eax, SYS_READ
+    mov ebx, STDIN
+    mov ecx, valor_1
+    mov edx, 0x3
+    int INT_80H
+
+    mov ecx, msg_valor_2
+    call imprimir_string
+    mov eax, SYS_READ
+    mov ebx, STDIN
+    mov ecx, valor_2
+    mov edx, 0x3
+    int INT_80H
+
+    mov ah, [opcao]
+    sub ah, "0"
+
+    cmp ah, 0x1
+    je adicao
+    
+    cmp ah, 0x2
+    je subtracao
+
+    cmp ah, 0x3
+    je divisao
+
+    cmp ah,0x4
+    je multiplicacao
 
 saida:
+    mov ecx, msg_fim
+    call imprimir_string
+
     mov eax, SYS_EXIT
     mov ebx, EXIT_SUCCESS
     int INT_80H
+
+mensagem_erro:
+    mov ecx, msg_erro
+    call imprimir_string
+    jmp saida
+
+adicao:
+    mov ecx, proc_adicao
+    call imprimir_string
+    jmp saida
+
+subtracao:
+    mov ecx, proc_subtracao
+    call imprimir_string
+    jmp saida
+
+divisao:
+    mov ecx, proc_divisao
+    call imprimir_string
+    jmp saida
+
+multiplicacao:
+    mov ecx, proc_multiplicacao
+    call imprimir_string
+    jmp saida

3e54d957cdbd4f864a8f57f8059a57bd1c2c3eaa
Danilo <danilogmoura@gmail.com>
2024-09-21 20:27:58 -0300

    Create arrays_1.asm

---
+++ Capitulo1_Conceitos_Iniciais/arrays_1.asm
@@ -0,0 +1,23 @@
+%include "../Biblioteca/bibliotecaE.inc"
+
+section .data
+    msg_1: db "Parte 1", LF, STR_END
+    msg_2: db "Parte 2", LF, STR_END
+    msg_3: db "Parte 3", LF, STR_END
+    msg_4: db "Parte 4", LF, STR_END
+
+section .text
+
+global _start
+
+_start:
+    mov eax, SYS_WRITE
+    mov ebx, STDOUT
+    mov ecx, msg_1
+    mov edx, 0x24               ; 36 decimal
+    int INT_80H
+
+saida:
+    mov eax, SYS_EXIT
+    mov ebx, EXIT_SUCCESS
+    int INT_80H

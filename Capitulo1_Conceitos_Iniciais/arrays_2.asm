e9d8901296e04ab528e5fec1520ac232914b28e5
Danilo <danilogmoura@gmail.com>
2024-09-22 01:25:01 -0300

    Create arrays_2.asm

---
+++ Capitulo1_Conceitos_Iniciais/arrays_2.asm
@@ -0,0 +1,20 @@
+%include "../Biblioteca/bibliotecaE.inc"
+
+section .data
+    array: dd 10, 20, 13, 14, 55
+
+section .text
+
+global _start
+
+_start:
+    mov eax, [array + 4 * 3]            ; Carrega o valor do quarto elemento do array em EAX
+    call int_to_string                  ; Converte o valor inteiro em EAX para uma string
+    call imprimir_string                ; Imprime a string resultante no termial
+    mov ecx, NEW_LINE
+    call imprimir_string
+
+saida:
+    mov eax, SYS_EXIT
+    mov ebx, EXIT_SUCCESS
+    int INT_80H

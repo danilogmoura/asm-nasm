5737867d4e78a06c505cae691436bd43ce58d86f
Danilo <danilogmoura@gmail.com>
2024-09-24 17:24:46 -0300

    Create questao.cpp

---
+++ Capitulo2_Integracao_C++/questao.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+extern "C" int Question(int a);
+
+int main()
+{
+    if (Question(27) == 1)
+    {
+        cout << "Número Par" << endl;
+    }
+    else
+    {
+        cout << "Número Impar" << endl;
+    }
+
+    return 0;
+}

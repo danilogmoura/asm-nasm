c5b9189a3b1b340b8080380305f8df1635fe4a44
Danilo <danilogmoura@gmail.com>
2024-09-24 00:57:55 -0300

    Create impar_par_desafio.asm

---
+++ Capitulo1_Conceitos_Iniciais/impar_par_desafio.asm
@@ -0,0 +1,33 @@
+%include "../Biblioteca/bibliotecaE.inc"
+
+section .data
+    msg_impar   dw      "impar", LF, STR_END
+    msg_par     dw      "par", LF, STR_END
+
+section .text
+
+global _start
+
+_start:
+    xor edx, edx
+    mov eax, 0x3
+    mov ebx, 0x2
+    div ebx
+
+    test edx, edx
+    jz numero_par
+
+    mov ecx, msg_impar
+
+imprimir:   
+    call imprimir_string
+    int INT_80H
+
+saida:
+    mov eax, SYS_EXIT
+    mov ebx, EXIT_SUCCESS
+    int INT_80H
+
+numero_par:
+    mov ecx, msg_par
+    jmp imprimir
